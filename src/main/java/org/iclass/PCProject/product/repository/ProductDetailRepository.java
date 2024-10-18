e83a0f9cce8b5ede246e973df0d94fcf49902945
jyjy <jungjuneyoung6@gmail.com>
2024-10-18 18:01:54 +0900

    241018 18

--- src/main/java/org/iclass/PCProject/product/repository/ProductDetailRepository.java
+++ src/main/java/org/iclass/PCProject/product/repository/ProductDetailRepository.java
@@ -4,7 +4,10 @@ import org.iclass.PCProject.product.entity.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, Integer> {
+    List<ProductDetail> findAllBypSeqOrderBySeq(int pSeq);
 
 }
