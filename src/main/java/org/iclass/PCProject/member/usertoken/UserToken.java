1afb113676815e5d4916e384a5b611c0b7dba7a3
ryu <himediazoom@gmail.com>
2024-10-24 11:29:51 +0900

    fix not works for camel to snake automatically ADD @Table(name=xxx_xxx) @Column(name=xxx_xxx) to all entity

--- src/main/java/org/iclass/PCProject/member/usertoken/UserToken.java
+++ src/main/java/org/iclass/PCProject/member/usertoken/UserToken.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="USER_TOKEN")
 public class UserToken {
 
     @Id
@@ -27,7 +28,7 @@ public class UserToken {
     private String token;
 
     @CreationTimestamp
-    @Column(nullable = false)
+    @Column(nullable = false, name="CREATED_AT")
     private LocalDateTime createdAt;
 
 
